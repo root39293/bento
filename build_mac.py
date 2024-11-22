3f6a714f13aed76c5445095bfb6fb0215fb1700f
faith6 <root39293@gmail.com>
2024-11-22 18:23:11 +0900

    fix: ignore, build process

--- build_mac.py
+++ build_mac.py
@@ -2,7 +2,7 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     'src/desktop.py',
-    '--name=KALIS_Assistant',
+    '--name=Bento_Assistant',
     '--onefile',
     '--windowed',
     '--add-data=static:static',
